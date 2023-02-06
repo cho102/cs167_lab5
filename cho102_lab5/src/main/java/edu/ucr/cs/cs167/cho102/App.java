47c4a14a81c5049309cf5caecde968c8fbd9210c
Cindy Ho <57569111+cho102@users.noreply.github.com>
2023-02-06 11:29:37 -0800

    Update App.java

--- cho102_lab5/src/main/java/edu/ucr/cs/cs167/cho102/App.java
+++ cho102_lab5/src/main/java/edu/ucr/cs/cs167/cho102/App.java
@@ -1,13 +1,14 @@
 package edu.ucr.cs.cs167.cho102;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class App {
+    public static void main(String[] args) {
+        final String inputPath = args[0];
+        try (JavaSparkContext spark = new JavaSparkContext("local[*]", "CS167-Lab5")) {
+            JavaRDD<String> logFile = spark.textFile(inputPath);
+            System.out.printf("Number of lines in the log file %d\n", logFile.count());
+        }
     }
 }
