b9a684cc118286e2efae55d447e8d05169cd72cb
Cindy Ho <57569111+cho102@users.noreply.github.com>
2023-02-07 10:38:13 -0800

    Update Filter.java

--- cho102_lab5/src/main/java/edu/ucr/cs/cs167/cho102/Filter.java
+++ cho102_lab5/src/main/java/edu/ucr/cs/cs167/cho102/Filter.java
@@ -7,14 +7,17 @@ import org.apache.spark.api.java.JavaSparkContext;
 public class App {
     public static void main(String[] args) {
         final String inputPath = args[0];
+        final String desiredCode = "200";
         SparkConf conf = new SparkConf();
         if (!conf.contains("spark.master"))
             conf.setMaster("local[*]");
         System.out.printf("Using Spark master '%s'\n", conf.get("spark.master"));
         conf.setAppName("CS167-Lab5");
         try (JavaSparkContext spark = new JavaSparkContext(conf)) {
-            JavaRDD<String> logFile = spark.textFile(inputPath);
-            System.out.printf("Number of lines in the log file %d\n", logFile.count());
+//             JavaRDD<String> logFile = spark.textFile(inputPath);
+//             System.out.printf("Number of lines in the log file %d\n", logFile.count());
+            JavaRDD<String> matchingLines = logFile.filter(line -> line.split("\t")[5].equals(desiredCode));
+            System.out.printf("The file '%s' contains %d lines with response code %s\n", inputPath, matchingLines.count(), desiredCode);
         }
     }
 }
